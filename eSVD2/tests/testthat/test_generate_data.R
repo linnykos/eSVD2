e4e1261968feb34d4c84bb4eba24f085aade85b1
Kevin Lin <klin1234@gmail.com>
2020-08-02 16:43:20 -0400

    Finished adding tests for generating data

--- eSVD2/tests/testthat/test_generate_data.R
+++ eSVD2/tests/testthat/test_generate_data.R
@@ -11,6 +11,33 @@ test_that("generate_data works", {
  expect_true(all(dim(res) == dim(nat_mat)))
 })
 
+############
+
+test_that("generate_data works for gaussian", {
+ set.seed(5)
+ nat_mat <- matrix(log(c(11:40)), 5, 6)
+ res <- generate_data(nat_mat, family = "gaussian", nuisance_param_vec = 1)
+ 
+ expect_true(is.matrix(res))
+ expect_true(all(dim(res) == dim(nat_mat)))
+})
+
+test_that("generate_data has the correct mean and variance for gaussian", {
+ set.seed(5)
+ canon_vec <- seq(10, 50, length.out = 5)
+ nat_mat <- sapply(canon_vec, function(x){rep(x, 1000)})
+ res <- generate_data(nat_mat, family = "gaussian", nuisance_param_vec = 1)
+ 
+ mean_vec <- colMeans(res)
+ expect_true(all(abs(mean_vec - canon_vec) <= 2))
+ 
+ var_vec <- apply(res, 2, stats::var)
+ expect_true(all(abs(var_vec - 1) <= 2))
+})
+
+
+############
+
 test_that("generate_data works for poisson", {
  set.seed(5)
  nat_mat <- matrix(log(c(11:40)), 5, 6)
@@ -19,6 +46,21 @@ test_that("generate_data works for poisson", {
  expect_true(is.matrix(res))
  expect_true(all(dim(res) == dim(nat_mat)))
 })
+
+test_that("generate_data has the correct mean and variance for poisson", {
+ set.seed(5)
+ canon_vec <- seq(10, 50, length.out = 5)
+ nat_mat <- sapply(log(canon_vec), function(x){rep(x, 1000)})
+ res <- generate_data(nat_mat, family = "poisson")
+ 
+ mean_vec <- colMeans(res)
+ expect_true(all(abs(mean_vec - canon_vec) <= 2))
+ 
+ var_vec <- apply(res, 2, stats::var)
+ expect_true(all(abs(var_vec - canon_vec) <= 2))
+})
+
+############
 
 test_that("generate_data works for curved gaussian", {
  set.seed(5)
@@ -29,6 +71,21 @@ test_that("generate_data works for curved gaussian", {
  expect_true(all(dim(res) == dim(nat_mat)))
 })
 
+test_that("generate_data has the correct mean and variance for curved guassian", {
+ set.seed(5)
+ canon_vec <- seq(10, 50, length.out = 5)
+ nat_mat <- sapply(1/canon_vec, function(x){rep(x, 1000)})
+ res <- generate_data(nat_mat, family = "curved_gaussian", nuisance_param_vec = 2)
+ 
+ mean_vec <- colMeans(res)
+ expect_true(all(abs(mean_vec - canon_vec) <= 2))
+ 
+ sd_vec <- apply(res, 2, stats::sd)
+ expect_true(all(abs(sd_vec - canon_vec/2) <= 2))
+})
+
+############
+
 test_that("generate_data works for exponential", {
  set.seed(5)
  nat_mat <- matrix(-c(1:30), 5, 6)
@@ -38,6 +95,21 @@ test_that("generate_data works for exponential", {
  expect_true(all(dim(res) == dim(nat_mat)))
 })
 
+test_that("generate_data has the correct mean and variance for curved guassian", {
+ set.seed(5)
+ canon_vec <- seq(10, 50, length.out = 5)
+ nat_mat <- sapply(-1/canon_vec, function(x){rep(x, 2000)})
+ res <- generate_data(nat_mat, family = "exponential")
+ 
+ mean_vec <- colMeans(res)
+ expect_true(all(abs(mean_vec - canon_vec) <= 2))
+ 
+ sd_vec <- apply(res, 2, stats::sd)
+ expect_true(all(abs(sd_vec - canon_vec) <= 3))
+})
+
+############
+
 test_that("generate_data works for neg_binom", {
  set.seed(5)
  nat_mat <- matrix(-1/c(1:30), 5, 6)
@@ -45,4 +117,18 @@ test_that("generate_data works for neg_binom", {
  
  expect_true(is.matrix(res))
  expect_true(all(dim(res) == dim(nat_mat)))
+})
+
+test_that("generate_data has the correct mean and variance for neg_binom", {
+ set.seed(5)
+ canon_vec <- -1/seq(10, 50, length.out = 5)
+ nat_mat <- sapply(canon_vec, function(x){rep(x, 2000)})
+ nuisance_param_val <- 10
+ res <- generate_data(nat_mat, family = "neg_binom", nuisance_param_vec = nuisance_param_val)
+ 
+ mean_vec <- colMeans(res)
+ expect_true(all(abs(mean_vec - nuisance_param_val*exp(canon_vec)/(1-exp(canon_vec))) <= 5))
+ 
+ sd_vec <- apply(res, 2, stats::sd)
+ expect_true(all(abs(sd_vec - sqrt(nuisance_param_val*exp(canon_vec)/(1-exp(canon_vec))^2)) <= 10))
 })
