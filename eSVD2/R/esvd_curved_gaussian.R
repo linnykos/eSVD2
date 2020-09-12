a1673d778b1112a68833735308f132d0034351d3
Yixuan Qiu <yixuanq@gmail.com>
2020-09-12 02:44:13 -0400

    formatting, documentation, and other tweaks

--- eSVD2/R/esvd_curved_gaussian.R
+++ eSVD2/R/esvd_curved_gaussian.R
@@ -1,50 +1,70 @@
-# distribution: one-parameter Gaussian where sd = mean/scalar
-# natural parameter: m_{ij} = u_i^Tv_j
-# relation to canonical parameters: m_{ij} = 1/mu_{ij}
-# optimization problem: -log(m_{ij}) - scalar^2*a_{ij}^2*(-m_{ij}^2)/2 - scalar^2*a_{ij}*m_{ij}
+# Distribution: one-parameter Gaussian where sd = mean/scalar
+# Natural parameter: m_{ij} = u_i^Tv_j
+# Relation to canonical parameters: m_{ij} = 1/mu_{ij}
+# Optimization problem: -log(m_{ij}) - scalar^2*a_{ij}^2*(-m_{ij}^2)/2 - scalar^2*a_{ij}*m_{ij}
 
-.evaluate_objective.curved_gaussian <- function(dat, u_mat, v_mat, scalar = 2, ...){
-  stopifnot(ncol(u_mat) == ncol(v_mat), nrow(u_mat) == nrow(dat), nrow(v_mat) == ncol(dat))
+.evaluate_objective.curved_gaussian <- function(dat, u_mat, v_mat, scalar = 2, ...) {
+    # Check dimensions
+    n <- nrow(dat)
+    p <- ncol(dat)
+    stopifnot(
+        ncol(u_mat) == ncol(v_mat),
+        nrow(u_mat) == n,
+        nrow(v_mat) == p
+    )
 
-  n <- nrow(dat); p <- ncol(dat)
-  nat_mat <- u_mat %*% t(v_mat)
-  stopifnot(all(nat_mat > 0))
+    # Compute natural parameters
+    nat_mat <- tcrossprod(u_mat, v_mat)
+    stopifnot(all(nat_mat > 0))
 
-  idx <- which(!is.na(dat))
+    # Only compute likelihood on non-missing data
+    idx <- which(!is.na(dat))
+    stopifnot(length(idx) > 0)
 
-  1/(n*p) * sum(-log(nat_mat[idx]) -
-        nat_mat[idx]*dat[idx]*scalar^2 +
-        nat_mat[idx]^2*dat[idx]^2*scalar^2/2)
+    nat_vals <- nat_mat[idx]
+    dat_vals <- dat[idx]
+    negloglik <- -log(nat_vals) +
+        scalar^2 * dat_vals^2 * nat_vals^2 / 2 -
+        scalar^2 * dat_vals * nat_vals
+    sum(negloglik) / n / p
 }
 
-.evaluate_objective_single.curved_gaussian <- function(dat_vec, current_vec, other_mat, n, p,
-                                                scalar = 2, ...){
-  stopifnot(length(current_vec) == ncol(other_mat), nrow(other_mat) == length(dat_vec))
+.evaluate_objective_single.curved_gaussian <- function(dat_vec, current_vec, other_mat, scalar = 2, ...) {
+    stopifnot(
+        length(current_vec) == ncol(other_mat),
+        length(dat_vec) == nrow(other_mat)
+    )
 
-  pred_vec <- other_mat %*% current_vec
-  stopifnot(all(pred_vec > 0))
+    nat_vec <- c(other_mat %*% current_vec)
+    stopifnot(all(nat_vec > 0))
+    idx <- which(!is.na(dat_vec))
+    stopifnot(length(idx) > 0)
 
-  idx <- which(!is.na(dat_vec))
-
-  1/(n*p) * sum(-log(pred_vec[idx]) -
-        pred_vec[idx]*dat_vec[idx]*scalar^2 +
-        pred_vec[idx]^2*dat_vec[idx]^2*scalar^2/2)
+    nat_vals <- nat_vec[idx]
+    dat_vals <- dat_vec[idx]
+    negloglik <- -log(nat_vals) +
+        scalar^2 * dat_vals^2 * nat_vals^2 / 2 -
+        scalar^2 * dat_vals * nat_vals
+    sum(negloglik) / length(dat_vec)
 }
 
-.gradient_vec.curved_gaussian <- function(dat_vec, current_vec, other_mat, n, p, scalar = 2, ...){
-  stopifnot(length(current_vec) == ncol(other_mat), nrow(other_mat) == length(dat_vec))
+.gradient_vec.curved_gaussian <- function(dat_vec, current_vec, other_mat, scalar = 2, ...) {
+    stopifnot(
+        length(current_vec) == ncol(other_mat),
+        length(dat_vec) == nrow(other_mat)
+    )
 
-  pred_vec <- other_mat %*% current_vec
-  stopifnot(all(pred_vec > 0))
+    nat_vec <- c(other_mat %*% current_vec)
+    stopifnot(all(nat_vec > 0))
+    idx <- which(!is.na(dat_vec))
+    stopifnot(length(idx) > 0)
 
-  idx <- which(!is.na(dat_vec))
-
-  tmp <- sapply(idx, function(j){
-    other_mat[j,,drop=F]*(- 1/pred_vec[j] - dat_vec[j]*scalar^2 +
-                          pred_vec[j]*scalar^2*dat_vec[j]^2)
-  })
-
-  if(is.matrix(tmp)) 1/(n*p) * rowSums(tmp) else 1/(n*p) * sum(tmp)
+    nat_vals <- nat_vec[idx]
+    dat_vals <- dat_vec[idx]
+    other_vals <- other_mat[idx, , drop = FALSE]
+    grad <- other_vals * (-1 / nat_vals + scalar^2 * dat_vals^2 * nat_vals -
+                              scalar^2 * dat_vals)
+    colSums(grad) / length(dat_vec)
 }
 
 .evaluate_objective_mat.curved_gaussian <- function(dat, nat_mat, scalar = 2, ...){
@@ -65,5 +85,3 @@
 
   (-1/(nat_mat) - scalar^2*dat + scalar^2*dat^2*nat_mat)/(n*p)
 }
-
-
--- eSVD2/R/generics.R
+++ eSVD2/R/generics.R
@@ -1,40 +1,64 @@
-.evaluate_objective <- function (dat, u_mat, v_mat, ...) {
-  stopifnot(is.matrix(dat), nrow(dat) == nrow(u_mat), ncol(dat) == nrow(v_mat),
-            ncol(u_mat) == ncol(v_mat))
+# Natural parameter: m_{ij} = u_i^Tv_j
+# Data: a_{ij} ~ p(x|m_{ij})
+#
+# This function computes the negative log-likelihood value given the data
+# `dat` may contain missing values
+.evaluate_objective <- function(dat, u_mat, v_mat, ...) {
+    stopifnot(
+        is.matrix(dat),
+        nrow(dat) == nrow(u_mat),
+        ncol(dat) == nrow(v_mat),
+        ncol(u_mat) == ncol(v_mat)
+    )
 
-  UseMethod(".evaluate_objective")
+    UseMethod(".evaluate_objective")
 }
 
-.evaluate_objective.default <- function(dat, u_mat, v_mat, ...){
-  .evaluate_objective.exponential(dat, u_mat, v_mat, ...)
+.evaluate_objective.default <- function(dat, u_mat, v_mat, ...) {
+    .evaluate_objective.exponential(dat, u_mat, v_mat, ...)
 }
 
 ##########
 
-.evaluate_objective_single <- function (dat_vec, current_vec, other_mat, n, p, ...) {
-  stopifnot(!is.matrix(dat_vec))
-  stopifnot(length(current_vec) == ncol(other_mat))
-  stopifnot(length(dat_vec) == nrow(other_mat))
+# This function computes the objective function on one row/column of data
+# For example,
+#   dat_vec = a_{i*} = (a_{i1}, ..., a_{ip})
+#   current_vec = u_i
+#   other_mat = V
+# Or,
+#   dat_vec = a_{*j} = (a_{1j}, ..., a_{nj})
+#   current_vec = v_j
+#   other_mat = U
 
-  UseMethod(".evaluate_objective_single")
+.evaluate_objective_single <- function(dat_vec, current_vec, other_mat, ...) {
+    stopifnot(
+        !is.matrix(dat_vec),
+        length(current_vec) == ncol(other_mat),
+        length(dat_vec) == nrow(other_mat)
+    )
+
+    UseMethod(".evaluate_objective_single")
 }
 
-.evaluate_objective_single.default <- function(dat_vec, current_vec, other_mat, n, p, ...){
-  .evaluate_objective_single.exponential(dat_vec, current_vec, other_mat, n, p, ...)
+.evaluate_objective_single.default <- function(dat_vec, current_vec, other_mat, ...) {
+    .evaluate_objective_single.exponential(dat_vec, current_vec, other_mat, ...)
 }
 
 ###########
 
-.gradient_vec <- function (dat_vec, current_vec, other_mat, ...) {
-  stopifnot(!is.matrix(dat_vec))
-  stopifnot(length(current_vec) == ncol(other_mat))
-  stopifnot(length(dat_vec) == nrow(other_mat))
+# The gradient of .evaluate_objective_single with respect to current_vec
+.gradient_vec <- function(dat_vec, current_vec, other_mat, ...) {
+    stopifnot(
+        !is.matrix(dat_vec),
+        length(current_vec) == ncol(other_mat),
+        length(dat_vec) == nrow(other_mat)
+    )
 
-  UseMethod(".gradient_vec")
+    UseMethod(".gradient_vec")
 }
 
-.gradient_vec.default <- function(dat_vec, current_vec, other_mat, n, p, ...){
-  .gradient_vec.exponential(dat_vec, current_vec, other_mat, n, p, ...)
+.gradient_vec.default <- function(dat_vec, current_vec, other_mat, ...) {
+    .gradient_vec.exponential(dat_vec, current_vec, other_mat, ...)
 }
 
 ###########
