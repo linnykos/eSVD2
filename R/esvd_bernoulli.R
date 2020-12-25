95b0f02763cdfe9eb88be65b47040239e4f12d3b
Yixuan Qiu <yixuanq@gmail.com>
2020-12-25 19:38:30 +0800

    finish Bernoulli distribution

--- R/esvd_bernoulli.R
+++ R/esvd_bernoulli.R
@@ -1,47 +1,46 @@
 # Distribution: Bernoulli
-## NEED TO BE IMPROVED
 
 .evaluate_objective.bernoulli <- function(
-    dat, u_mat, v_mat, nuisance_param_vec, library_size_vec, ...
+  dat, x_mat, y_mat, nuisance_param_vec, library_size_vec, ...
 ) {
-    # Check dimensions
-    n <- nrow(dat)
-    p <- ncol(dat)
-    stopifnot(
-        ncol(u_mat) == ncol(v_mat),
-        nrow(u_mat) == n,
-        nrow(v_mat) == p
-    )
+  # Check dimensions
+  n <- nrow(dat)
+  p <- ncol(dat)
+  stopifnot(
+    ncol(x_mat) == ncol(y_mat),
+    nrow(x_mat) == n,
+    nrow(y_mat) == p
+  )
 
-    # Compute natural parameters
-    nat_mat <- tcrossprod(u_mat, v_mat)
+  # Compute natural parameters
+  nat_mat <- tcrossprod(x_mat, y_mat)
 
-    # Only compute likelihood on non-missing data
-    idx <- which(!is.na(dat))
-    stopifnot(length(idx) > 0)
+  # Only compute likelihood on non-missing data
+  idx <- which(!is.na(dat))
+  stopifnot(length(idx) > 0)
 
-    nat_vals <- nat_mat[idx]
-    dat_vals <- dat[idx]
-    negloglik <- log(1 + exp(nat_vals)) - nat_vals * dat_vals
-    sum(negloglik) / n / p
+  nat_vals <- nat_mat[idx]
+  dat_vals <- dat[idx]
+  negloglik <- log(1 + exp(nat_vals)) - nat_vals * dat_vals
+  mean(negloglik)
 }
 
 .evaluate_objective_single.bernoulli <- function(
-    current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
+  current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
 ) {
-    stopifnot(
-        length(current_vec) == ncol(other_mat),
-        length(dat_vec) == nrow(other_mat)
-    )
+  stopifnot(
+    length(current_vec) == ncol(other_mat),
+    length(dat_vec) == nrow(other_mat)
+  )
 
-    nat_vec <- c(other_mat %*% current_vec)
-    idx <- which(!is.na(dat_vec))
-    stopifnot(length(idx) > 0)
+  nat_vec <- c(other_mat %*% current_vec)
+  idx <- which(!is.na(dat_vec))
+  stopifnot(length(idx) > 0)
 
-    nat_vals <- nat_vec[idx]
-    dat_vals <- dat_vec[idx]
-    negloglik <- log(1 + exp(nat_vals)) - nat_vals * dat_vals
-    sum(negloglik) / length(dat_vec)
+  nat_vals <- nat_vec[idx]
+  dat_vals <- dat_vec[idx]
+  negloglik <- log(1 + exp(nat_vals)) - nat_vals * dat_vals
+  mean(negloglik)
 }
 
 # f(x) = log(1 + exp(x))
@@ -50,59 +49,58 @@
 #        = f'(x) * [1 - f'(x)]
 
 .gradient_vec.bernoulli <- function(
-    current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
+  current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
 ) {
-    stopifnot(
-        length(current_vec) == ncol(other_mat),
-        length(dat_vec) == nrow(other_mat)
-    )
+  stopifnot(
+    length(current_vec) == ncol(other_mat),
+    length(dat_vec) == nrow(other_mat)
+  )
 
-    nat_vec <- c(other_mat %*% current_vec)
-    idx <- which(!is.na(dat_vec))
-    stopifnot(length(idx) > 0)
+  nat_vec <- c(other_mat %*% current_vec)
+  idx <- which(!is.na(dat_vec))
+  stopifnot(length(idx) > 0)
 
-    nat_vals <- nat_vec[idx]
-    dat_vals <- dat_vec[idx]
-    other_vals <- other_mat[idx, , drop = FALSE]
-    grad <- other_vals * (stats::plogis(nat_vals) - dat_vals)
+  nat_vals <- nat_vec[idx]
+  dat_vals <- dat_vec[idx]
+  other_vals <- other_mat[idx, , drop = FALSE]
+  grad <- other_vals * (stats::plogis(nat_vals) - dat_vals)
 
-    colSums(grad) / length(dat_vec)
+  colMeans(grad)
 }
 
 .hessian_vec.bernoulli <- function(
-    current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
+  current_vec, other_mat, dat_vec, nuisance_param_vec, library_size, ...
 ) {
-    stopifnot(
-        length(current_vec) == ncol(other_mat),
-        length(dat_vec) == nrow(other_mat)
-    )
+  stopifnot(
+    length(current_vec) == ncol(other_mat),
+    length(dat_vec) == nrow(other_mat)
+  )
 
-    nat_vec <- c(other_mat %*% current_vec)
-    idx <- which(!is.na(dat_vec))
-    stopifnot(length(idx) > 0)
+  nat_vec <- c(other_mat %*% current_vec)
+  idx <- which(!is.na(dat_vec))
+  stopifnot(length(idx) > 0)
 
-    nat_vals <- nat_vec[idx]
-    dat_vals <- dat_vec[idx]
-    other_vals <- other_mat[idx, , drop = FALSE]
+  nat_vals <- nat_vec[idx]
+  dat_vals <- dat_vec[idx]
+  other_vals <- other_mat[idx, , drop = FALSE]
 
-    fpx = stats::plogis(nat_vals)
-    fppx = fpx * (1 - fpx)
-    term1 <- t(other_vals) %*% diag(fppx) %*% other_vals
-
-    term1 / length(dat_vec)
+  fpx = stats::plogis(nat_vals)
+  fppx = fpx * (1 - fpx)
+  hess <- crossprod(.mult_vec_mat(fppx, other_vals), other_vals) / length(idx)
+  hess
 }
 
 .feasibility.bernoulli <- function(current_vec, other_mat, ...) {
-    TRUE
+  TRUE
 }
 
 .bernoulli <- structure(
-    list(
-        objfn_all = .evaluate_objective.bernoulli,
-        objfn     = .evaluate_objective_single.bernoulli,
-        grad      = .gradient_vec.bernoulli,
-        hessian   = .hessian_vec.bernoulli,
-        feas      = .feasibility.bernoulli
-    ),
-    class = "esvd_family"
+  list(
+    objfn_all = .evaluate_objective.bernoulli,
+    objfn     = .evaluate_objective_single.bernoulli,
+    grad      = .gradient_vec.bernoulli,
+    hessian   = .hessian_vec.bernoulli,
+    feas      = .feasibility.bernoulli
+  ),
+  class = "esvd_family"
 )
