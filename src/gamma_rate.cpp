0b9f90e6d5cd4411708c951faa447096101f4c11
Yixuan Qiu <yixuanq@gmail.com>
2022-01-26 15:51:31 +0800

    improved root finding

--- src/gamma_rate.cpp
+++ src/gamma_rate.cpp
@@ -44,51 +44,71 @@ public:
         double dl = 0.0, d2l = 0.0;
         for(int i = 0; i < m_n; i++)
         {
-            const double xi = m_x[i];
-            const double mui = m_mu[i];
-            const double mui2 = mui * mui;
-            const double si = m_s[i];
+            const double xi = m_x[i], mui = m_mu[i], si = m_s[i];
             const double mb = mui * b;
             const double mbx = mb + xi;
+            const double msx = mui * si - xi;
             const double bs = b + si;
-            const double dli = mui * (std::log(b) + 1.0) +
-                mui * (R::digamma(mbx) - R::digamma(mb)) -
-                mui * std::log(bs) - mbx / bs;
-            const double d2li = mui / b + mui2 * (R::trigamma(mbx) - R::trigamma(mb)) -
-                mui / bs - (mui * si - xi) / bs / bs;
+            // Two log() calls for numerical stability
+            const double dli = mui * (std::log(b) - std::log(bs)) +
+                mui * (R::digamma(mbx) - R::digamma(mb)) + msx / bs;
+            const double d2li = mui / b - mui / bs +
+                mui * mui * (R::trigamma(mbx) - R::trigamma(mb)) -
+                msx / bs / bs;
             dl += dli;
             d2l += d2li;
         }
+        Rcpp::Rcout << "b = " << b << ", dl = " << dl << ", d2l = " << d2l << std::endl;
         return std::make_pair(dl / m_n, d2l / m_n);
     }
 };
 
-// Note that [l(b)]' -> 0 when b -> +Inf, so we need to initialize b
-// from a small value
+// A local maximum of l(b) should satisfy [l(b)]'=0 and [l(b)]''<0
+// Note that [l(b)]' -> -0 and [l(b)]'' -> +0 when b -> +Inf,
+// so we need to initialize b from a small value
 //
-// We first compute the result x* within [lb, ub] given initial value x0
-// If [l(x*)]'' > 0, it means it is not a local maximum
-// In this case, we set x0 <- x0/2, lb <- lb/2, and recompute the root
+// First we choose a proper upper bound for b*, the (local) maximum
+// Let ub=max(s), if [l(ub)]''<0, then use ub;
+// otherwise repeatedly multiply ub by a constant 0<gamma<1, until
+// [l(ub)]''>0 and [l(gamma*ub)]''<0
+//
+// We then compute the result b* within [lb, ub] given initial value b0
+// If [l(b*)]'' > 0, it means it is not a local maximum
+// In this case, we set x0 <- gamma*x0, lb <- gamma*lb, and recompute the root
 
 // [[Rcpp::export]]
 double gamma_rate(NumericVector x, NumericVector mu, NumericVector s)
 {
     // Function object
     LogLikDeriv deriv(x, mu, s);
-    // Initial guess
-    double guess = 1.0;
+
+    // Find a proper upper bound
+    // gamma is the decrease factor, max_try is the maximum number of tries
+    double ub = Rcpp::max(s);
+    const double gamma = 0.5;
+    const int max_try = 10;
+    std::pair<double, double> dvals = deriv(ub);
+    if(dvals.second > 0.0)
+    {
+        for(int i = 0; i < max_try; i++)
+        {
+            const double new_ub = gamma * ub;
+            std::pair<double, double> new_dvals = deriv(new_ub);
+            if(new_dvals.second <= 0.0)
+                break;
+            ub = new_ub;
+        }
+    }
+
     // Lower bound
     double lb = 1e-6;
-    // Upper bound
-    double ub = Rcpp::max(s);
-    ub = std::max(ub, 1.1 * guess);
+    // Initial guess
+    double guess = std::min(1.0, 0.5 * (lb + ub));
     // Precision parameter
     const int digits = std::numeric_limits<double>::digits;
     int get_digits = static_cast<int>(digits * 0.5);
-    // Maximum number of iterations
+    // Maximum number of iterations for Newton's method
     const std::uintmax_t max_iter = 10;
-    // Maximum number of tries
-    const int max_try = 10;
 
     double res = 0.0;
     for(int i = 0; i < max_try; i++)
@@ -96,11 +116,11 @@ double gamma_rate(NumericVector x, NumericVector mu, NumericVector s)
         std::uintmax_t iter = max_iter;
         res = newton_raphson_iterate(deriv, guess, lb, ub, get_digits, iter);
         std::pair<double, double> dvals = deriv(res);
-        // If d2l > 0, make guess smaller and try again
+        // If [l(b*)]'' > 0, make guess smaller and try again
         if(dvals.second > 0.0)
         {
-            guess *= 0.5;
-            lb *= 0.5;
+            guess *= gamma;
+            lb *= gamma;
         } else {
             return res;
         }
