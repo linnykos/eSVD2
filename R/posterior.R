21caa7ef02a968038714df09e70e3876c5084633
linnykos <klin1234@gmail.com>
2022-06-13 21:15:25 -0400

    Minor changes

--- R/posterior.R
+++ R/posterior.R
@@ -123,7 +123,7 @@ compute_posterior.default <- function(input_obj,
   idx_vec <- c(case_control_idx, library_idx)
 
   nat_mat1 <- tcrossprod(esvd_res$x_mat, esvd_res$y_mat)
-  nat_mat2 <- tcrossprod(covariates, esvd_res$z_mat)
+  nat_mat2 <- tcrossprod(covariates[,-library_idx], esvd_res$z_mat[,-library_idx])
   nat_mat_nolib <- nat_mat1 + nat_mat2
   mean_mat_nolib <- exp(nat_mat_nolib)
   library_mat <- exp(tcrossprod(
@@ -144,14 +144,11 @@ compute_posterior.default <- function(input_obj,
     AplusAlpha <- exp(tmp - nat_mat_confounder)
   }
 
-  image(AplusAlpha, main = "Alpha")
   AplusAlpha <- pmin(AplusAlpha, alpha_max)
 
   print(quantile(library_mat))
   SplusBeta <- sweep(library_mat, MARGIN = 2,
                      STATS = nuisance_vec, FUN = "+")
-  image(SplusBeta, main = "Beta")
-  #print(quantile(SplusBeta))
   posterior_mean_mat <- AplusAlpha/SplusBeta
   posterior_var_mat <- AplusAlpha/SplusBeta^2
 
