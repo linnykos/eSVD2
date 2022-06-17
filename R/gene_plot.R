c47f2a1cabfb55aab9712d2f7cdb1858242af924
linnykos <klin1234@gmail.com>
2022-06-17 17:35:27 -0400

    Fixing another typo

--- R/gene_plot.R
+++ R/gene_plot.R
@@ -67,9 +67,9 @@ gene_plot <- function(input_obj,
                        pch = 16, cex = 2.5)
 
       for(idx in shuff_idx){
-        graphics::rug(vec_1[idx,1], col = color_vec[idx], side = rug_x_side,
+        graphics::rug(vec_1[idx], col = color_vec[idx], side = rug_x_side,
                       lwd = 2)
-        graphics::rug(vec_2[idx,2], col = color_vec[idx], side = rug_y_side,
+        graphics::rug(vec_2[idx], col = color_vec[idx], side = rug_y_side,
                       lwd = 2)
       }
     }
